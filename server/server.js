22729e04c01b73d0f4c4b60e4888a5d426e457c9
Sarthak-145 <sarthakkanawade145@gmail.com>
2025-06-15 21:35:51 +0530

    Add db config, created basic server structure

--- server/server.js
+++ server/server.js
@@ -1,32 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+// Connect to Mongo
+connectDB();
 
 // Middleware
-app.use(cors());
-app.use(express.json()); // so we can receive JSON in requests
+app.use(express.json());
 
-// Basic test route
-app.get('/', (req, res) => {
-  res.send('API is working 🎯');
-});
+// Routes
+app.get('/', (req, res) => res.send('API is working 🎯'));
+app.use('/api/auth', require('./routes/authRoutes'));
 
-// Connect to MongoDB (we’ll add URI soon)
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log('MongoDB connected ✅');
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error('MongoDB connection failed ❌', err.message);
-  });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
